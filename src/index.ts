d673802d908e828772fccd6c90348a1e5cacc351
Raul Rodrigues <64855653+luarrekcah@users.noreply.github.com>
2025-02-26 12:45:27 -0500

    first commit

---
+++ src/index.ts
@@ -0,0 +1,73 @@
+import makeWASocket, {
+  Browsers,
+  useMultiFileAuthState,
+  DisconnectReason,
+  WAMessage,
+} from "baileys";
+import qrcode from "qrcode-terminal";
+import { Boom } from "@hapi/boom";
+import { logger } from "./utils/logger";
+import { FormattedMessage, getMessage } from "./utils/message";
+import MessageHandler from "./handlers/message";
+
+export const initWASocket = async (): Promise<void> => {
+  const { state, saveCreds } = await useMultiFileAuthState("auth");
+
+  // @ts-ignore
+  const sock = makeWASocket({
+    auth: state,
+    browser: Browsers.appropriate("Desktop"),
+    printQRInTerminal: false,
+  });
+
+  sock.ev.on("connection.update", ({ connection, lastDisconnect, qr }: any) => {
+    logger.info(
+      `Socket Connection Update: ${connection || ""} ${lastDisconnect || ""}`
+    );
+
+    switch (connection) {
+      case "close":
+        logger.error("Conexão fechada");
+        // Remover o bot/deletar dados se necessário
+        const shouldReconnect =
+          (lastDisconnect.error as Boom)?.output?.statusCode !==
+          DisconnectReason.loggedOut;
+
+        if (shouldReconnect) {
+          initWASocket();
+        }
+        break;
+      case "open":
+        logger.info("Bot Conectado");
+        // Remover o bot/deletar dados se necessário
+        break;
+    }
+
+    if (qr !== undefined) {
+      qrcode.generate(qr, { small: true });
+    }
+  });
+
+  sock.ev.on("messages.upsert", ({ messages }: { messages: WAMessage[] }) => {
+    for (let index = 0; index < messages.length; index++) {
+      const message = messages[index];
+
+      const isGroup = message.key.remoteJid?.endsWith("@g.us");
+      const isStatus = message.key.remoteJid === "status@broadcast";
+
+      if (isGroup || isStatus) return;
+
+      // @ts-ignore
+      const formattedMessage: FormattedMessage | undefined =
+        getMessage(message);
+      if (formattedMessage !== undefined) {
+        MessageHandler(sock, formattedMessage);
+      }
+    }
+  });
+
+  // Salvar as credenciais de autenticação
+  sock.ev.on("creds.update", saveCreds);
+};
+
+initWASocket();
