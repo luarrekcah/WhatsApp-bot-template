fd0402c7f3ee513d2ffab0acb3afa8b4681fb4fe
luarrekcah <raulrodriguesdemoraes@gmail.com>
2025-04-26 21:45:56 -0500

    obter versão mais recente + login por número (não passou nos testes, mas está implementado segundo as docs do baileys).

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import makeWASocket, {
   useMultiFileAuthState,
   DisconnectReason,
   WAMessage,
+  fetchLatestWaWebVersion,
 } from "baileys";
 import qrcode from "qrcode-terminal";
 import { Boom } from "@hapi/boom";
@@ -10,42 +11,81 @@ import { logger } from "./utils/logger";
 import { FormattedMessage, getMessage } from "./utils/message";
 import MessageHandler from "./handlers/message";
 
+/**
+ * A conexão com o número precisa de correção, mas implementei
+ * aqui segundo as docs.
+ */
+
+const CONNECTION_TYPE = "QR"; // "NUMBER" (se quiser usar o número para login)
+const PHONE_NUMBER = "556892000000"; // +55 (68) 9200-0000 -> 556892000000 (formato para número)
+const USE_LASTEST_VERSION = true;
+
 export const initWASocket = async (): Promise<void> => {
   const { state, saveCreds } = await useMultiFileAuthState("auth");
+
+  const { version, isLatest } = await fetchLatestWaWebVersion({});
+
+  if (USE_LASTEST_VERSION) {
+    logger.info(
+      `Versão atual do WaWeb: ${version.join(".")} | ${
+        isLatest ? "Versão mais recente" : "Está desatualizado"
+      }`
+    );
+  }
 
   // @ts-ignore
   const sock = makeWASocket({
     auth: state,
-    browser: Browsers.appropriate("Desktop"),
+    browser:
+      // @ts-ignore
+      CONNECTION_TYPE === "NUMBER"
+        ? Browsers.ubuntu("Chrome")
+        : Browsers.appropriate("Desktop"),
     printQRInTerminal: false,
+    version: USE_LASTEST_VERSION ? version : undefined,
+    defaultQueryTimeoutMs: 0,
   });
 
-  sock.ev.on("connection.update", ({ connection, lastDisconnect, qr }: any) => {
-    logger.info(
-      `Socket Connection Update: ${connection || ""} ${lastDisconnect || ""}`
-    );
-
-    switch (connection) {
-      case "close":
-        logger.error("Conexão fechada");
-        // Remover o bot/deletar dados se necessário
-        const shouldReconnect =
-          (lastDisconnect.error as Boom)?.output?.statusCode !==
-          DisconnectReason.loggedOut;
-
-        if (shouldReconnect) {
-          initWASocket();
-        }
-        break;
-      case "open":
-        logger.info("Bot Conectado");
-        break;
+  // @ts-ignore
+  if (CONNECTION_TYPE === "NUMBER" && !sock.authState.creds.registered) {
+    try {
+      const code = await sock.requestPairingCode(PHONE_NUMBER);
+      console.log(`Código de Pareamento: ${code}`);
+    } catch (error) {
+      logger.error("Erro ao obter o código.");
     }
+  }
 
-    if (qr !== undefined) {
-      qrcode.generate(qr, { small: true });
+  sock.ev.on(
+    "connection.update",
+    async ({ connection, lastDisconnect, qr }: any) => {
+      logger.info(
+        `Socket Connection Update: ${connection || ""} ${lastDisconnect || ""}`
+      );
+
+      switch (connection) {
+        case "close":
+          logger.error("Conexão fechada");
+          // Remover o bot/deletar dados se necessário
+          const shouldReconnect =
+            (lastDisconnect.error as Boom)?.output?.statusCode !==
+            DisconnectReason.loggedOut;
+
+            if (shouldReconnect) {
+              setTimeout(() => initWASocket(), 5000);  // Atraso de 5 segundos antes de reconectar
+            }
+          break;
+        case "open":
+          logger.info("Bot Conectado");
+          break;
+      }
+
+      // @ts-ignore
+      if (qr !== undefined && CONNECTION_TYPE === "QR") {
+        qrcode.generate(qr, { small: true });
+      }
     }
-  });
+  );
 
   sock.ev.on("messages.upsert", ({ messages }: { messages: WAMessage[] }) => {
     for (let index = 0; index < messages.length; index++) {
